f10c1d1451a227bfebd1700c7dcfa91fb8cf761f
nikrb <nik_rb@yahoo.com>
2017-06-23 11:08:49 +0100

    add required prop types for Cell

--- src/components/Board/Cell.js
+++ src/components/Board/Cell.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Cell extends React.Component {
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    colour: PropTypes.string.isRequired
+  };
   render = () => {
     const table_cell = {
       // border: "1px solid rgba( 127,127,127,0.8)",
